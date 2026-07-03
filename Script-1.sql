ca5e1a230b19014f10465ed2ca696b609b25d9e6
Avedik <43447356+Avedik@users.noreply.github.com>
2026-07-03 11:53:45 +0300

    Update Script-1.sql

--- Script-1.sql
+++ Script-1.sql
@@ -6,44 +6,21 @@
 Результаты должны быть отсортированы по количеству активностей в порядке убывания.
 */
 
-with activity_stats as (
-    select 
-        user_id, 
-        count(id) as act_count
+with user_activities_last_month as (
+    select user_id, count(id) as activity_count
     from user_activity
+    -- ТРЕБУЕТСЯ УТОЧНЕНИЕ ФРАЗЫ "за последний месяц":
     -- where activity_date >= current_date - interval '1 month'
-    where activity_date >= '2024-10-01' and activity_date <= '2024-10-31'
-    group by user_id
-),
-role_stats as (
-    select 
-        user_id, 
-        string_agg(role, ', ') as roles
-    from user_roles
+    where activity_date >= '2024-10-01' and activity_date < '2024-11-01'
     group by user_id
 )
 select 
     u.id,
     u.username,
-    coalesce(rs.roles, 'нет роли') as roles,
-    coalesce(ast.act_count, 0) as activity_count
+    string_agg(ur.role, ', ') as roles,
+    ua.activity_count
 from users u
-left join role_stats rs on u.id = rs.user_id
-left join activity_stats ast on u.id = ast.user_id
-order by activity_count desc;
-
--- ТРЕБУЕТСЯ УТОЧНЕНИЕ ФРАЗЫ "за последний месяц" 
-/*select
-    u.id,
-    u.username,
-    string_agg(distinct ur.role, ', ' order by ur.role) as roles,
-    count(distinct ua.id)                                as activity_count
-from users u
-left join user_roles ur
-       on ur.user_id = u.id
-left join user_activity ua
-       on ua.user_id = u.id
-      and ua.activity_date >= date_trunc('month', current_date - interval '1 month')
-      and ua.activity_date <  date_trunc('month', current_date)
-group by u.id, u.username
-order by activity_count desc, u.username;*/
+join user_activities_last_month ua on u.id = ua.user_id
+left join user_roles ur on u.id = ur.user_id
+group by u.id, u.username, ua.activity_count
+order by ua.activity_count desc;
